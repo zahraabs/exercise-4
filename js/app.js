0715b27ed16c3fe157a45e936bb23d6f1122aa6b
zahraabs <zahraabbasiun@gmail.com>
2023-07-08 13:41:40 +0330

    some changes added

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+// make variables
 var time = document.querySelector(".time .show");
 var start = document.querySelector(".buttons button:nth-child(1)");
 var stop = document.querySelector(".buttons button:nth-child(2)");
@@ -12,15 +13,20 @@ var timer = {
   milliSeconds: 0,
 };
 
+//make click event for start button
 start.addEventListener("click", function () {
+  //condition for clearing interval at the first
   if (int !== null) {
     clearInterval(int);
   }
+  //flag for showing list
   isShowed = true;
+  //start timer
   int = setInterval(displayTimer, 10);
 })
-
+// click event for stop button
 stop.addEventListener("click", function () {
+  //if flag is true show the list of stopping time
   if (isShowed == true) {
     list.style.display = "block";
     list.innerHTML += `<li>${paddingStart(timer.hours)} : ${paddingStart(timer.minutes)} : ${paddingStart(timer.seconds)} :${paddingStart(timer.milliSeconds)}</li>`;
@@ -30,8 +36,9 @@ stop.addEventListener("click", function () {
 
 })
 
-
+// click event for reset button
 reset.addEventListener("click", function () {
+  // reset all for new timer
   isShowed = false;
   clearInterval(int);
   time.innerHTML = `00 : 00 : 00 : 00 `;
@@ -45,7 +52,7 @@ reset.addEventListener("click", function () {
   list.innerHTML = [];
 })
 
-
+// function for displaying timer
 function displayTimer() {
   timer.milliSeconds += 1;
   if (timer.milliSeconds == 100) {
@@ -60,6 +67,7 @@ function displayTimer() {
       }
     }
   }
+  // make each part double digit
   var second = timer.seconds;
   var milliSecond = timer.milliSeconds;
   var minute = timer.minutes;
@@ -76,7 +84,7 @@ function displayTimer() {
   time.innerHTML = `${hour} : ${minute} : ${second} :${milliSecond}`;
 }
 
-
+// function for being in double digits with 0 in the list
 function paddingStart(time) {
   return String(time).padStart(2 , "0");
 }
